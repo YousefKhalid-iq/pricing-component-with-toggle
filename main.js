efd43b56961121c43e1e40d82814b62bb45889b8
Yousef Khalid <84613950+YousefKhalid-iq@users.noreply.github.com>
2021-09-23 05:49:12 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,54 @@
 const button = document.querySelector(".container");
 const toggle = document.querySelector(".toggle-btn");
 
+function  btnToggle() {
+  function toggleBasic() {
+		let x = document.getElementById("yearly-basic");
+		let y = document.getElementById("monthly-basic");
+
+		if (x.style.display === "block"){
+			x.style.display = "none";
+			y.style.display = "block";
+		} else {
+				x.style.display = "block";
+				y.style.display = "none";
+		}
+	}
+
+	function togglePro() {
+		let x = document.getElementById("yearly-pro");
+		let y = document.getElementById("monthly-pro");
+
+		if (x.style.display === "block"){
+			x.style.display = "none";
+			y.style.display = "block";
+		} else {
+				x.style.display = "block";
+				y.style.display = "none";
+		}
+	}
+
+	function toggleMaster() {
+		let x = document.getElementById("yearly-master");
+		let y = document.getElementById("monthly-master");
+
+		if (x.style.display === "block"){
+			x.style.display = "none";
+			y.style.display = "block";
+		} else {
+				x.style.display = "block";
+				y.style.display = "none";
+		}
+	}
+	toggleBasic();
+	togglePro();
+	toggleMaster();
+}
+
 button.addEventListener("click", function () {
 	toggle.classList.toggle('active');
 });
 
 
-// onclick="this.classList.toggle('active')
+
+
